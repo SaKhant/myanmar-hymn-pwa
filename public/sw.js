92b4fc7d5610593032216ed6ef483c28aedfad2a
SaKhant <196737716+SaKhant@users.noreply.github.com>
2026-08-15 15:43:29 +0700

    Filter categories to valid Myanmar hymns

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="hymn-house-v34";
+const CACHE="hymn-house-v35";
 const APP_SHELL=["/","/yp","/favorites","/settings","/categories","/icon-192.png","/icon-512.png","/splash-guitar.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
